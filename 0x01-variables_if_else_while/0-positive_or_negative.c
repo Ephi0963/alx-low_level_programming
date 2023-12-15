a15f4a8b8885973095b130572a6ad5edf89be755
ephi0963 <ephraimsolomon0101@gmail.com>
2023-12-15 01:53:26 -0800

    solo

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+include<stdio.h>
+/**
+ * main -main block
+ * Return: 0
+ */
+int main(void)
+{
+	if (n > 0)
+	{
+		printf("is positive");
+	}
+
+	else if (n == 0)
+	{
+		printf("is zero");
+	}
+
+	else if (n < 0)
+	{
+		printf("is negative");
+	}
+	return (0);
+}
