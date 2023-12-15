1463c542155633dc501f23aed2d3d953586981c0
ephi0963 <ephraimsolomon0101@gmail.com>
2023-12-15 07:46:34 -0800

    hello world

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main -main block
+ * Return: 0
+ */
+int main(void)
+{
+	char = beta;
 
+	beta = 'a';
+	while (beta <= 'z')
+	{
+		putchar(beta);
+		beta++;
+	}
+	beta = 'A';
+	while (beta <= 'Z')
+	{
+		putchar(beta);
+		beta++;
+	}
+
+	return (0);
+}
