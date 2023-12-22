d88bfe4e38a42a4c8c94739684d0e8ef8e264f57
ephi0963 <ephraimsolomon0101@gmail.com>
2023-12-22 01:11:44 -0800

    jiji

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - checkthe code
+ *
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
